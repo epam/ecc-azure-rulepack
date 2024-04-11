c880a97bfc6022f2318453a3607a1f4205b1511a
vit-corp <liashuk.corp@gmail.com>
2024-04-11 19:46:39 +0300

    new: add policy ecc-azure-025-cis_db_mysql_ssl

--- terraform/ecc-azure-025-cis_db_mysql_ssl/red/mysql_server.tf
+++ terraform/ecc-azure-025-cis_db_mysql_ssl/red/mysql_server.tf
@@ -1,20 +1,17 @@
-resource "azurerm_mysql_server" "this" {
+resource "azurerm_mysql_flexible_server" "this" {
   name                = "${var.prefix}-mysqlserver-red"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   administrator_login          = random_string.this.result
-  administrator_login_password = random_password.this.result
+  administrator_password       = random_password.this.result
+  sku_name                     = "GP_Standard_D2ds_v4"
+  }
 
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
-}
+  resource "azurerm_mysql_flexible_server_configuration" "this" {
+    name                       = "require_secure_transport"
+    resource_group_name        = azurerm_resource_group.this.name
+    server_name                = azurerm_mysql_flexible_server.this.name
+    value                      = "OFF"
+    depends_on = [ azurerm_mysql_flexible_server.this ]
+  }
