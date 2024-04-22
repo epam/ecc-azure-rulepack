4f24f635972fee8e0a41a1c1ba19a839d628f1af
vit-corp <liashuk.corp@gmail.com>
2024-04-22 20:45:54 +0300

    new: add policy ecc-azure-371-cis_mysql_audit_log_enabled

--- terraform/ecc-azure-371-cis_mysql_audit_log_enabled/green/mysql.tf
+++ terraform/ecc-azure-371-cis_mysql_audit_log_enabled/green/mysql.tf
@@ -1,27 +1,19 @@
-resource "azurerm_mysql_server" "this" {
-  name                = "${var.prefix}mysqlserver-green"
+resource "azurerm_mysql_flexible_server" "this" {
+  name                = "${var.prefix}-mysqlserver-green"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   administrator_login          = random_string.this.result
-  administrator_login_password = random_password.this.result
-
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  ssl_enforcement_enabled = true
-
-  identity {
-    type = "SystemAssigned"
-  }
+  administrator_password       = random_password.this.result
+  sku_name                     = "GP_Standard_D2ds_v4"
 
   tags = var.tags
 }
-
-resource "azurerm_mysql_configuration" "audit_log_enabled" {
-  name                = "audit_log_enabled"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = azurerm_mysql_server.this.name
-  value               = "on"
+  
+resource "azurerm_mysql_flexible_server_configuration" "this" {
+  name                       = "audit_log_enabled"
+  resource_group_name        = azurerm_resource_group.this.name
+  server_name                = azurerm_mysql_flexible_server.this.name
+  value                      = "ON"
+  depends_on = [ azurerm_mysql_flexible_server.this ]
 }
