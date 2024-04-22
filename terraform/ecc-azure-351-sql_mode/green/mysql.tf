b6897a54f011b81b14e2f17802a1d716b0de3303
vit-corp <liashuk.corp@gmail.com>
2024-04-22 20:45:25 +0300

    new: add policy ecc-azure-351-sql_mode

--- terraform/ecc-azure-351-sql_mode/green/mysql.tf
+++ terraform/ecc-azure-351-sql_mode/green/mysql.tf
@@ -1,27 +1,19 @@
-resource "azurerm_mysql_server" "this" {
-  name                = "${var.prefix}mysqlserver-green"
+resource "azurerm_mysql_flexible_server" "this" {
+  name                = "${var.prefix}-mysqlserver-green"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   administrator_login          = random_string.this.result
-  administrator_login_password = random_password.this.result
-
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  ssl_enforcement_enabled = true
-
-  identity {
-    type = "SystemAssigned"
-  }
+  administrator_password       = random_password.this.result
+  sku_name                     = "GP_Standard_D2ds_v4"
 
   tags = var.tags
 }
-
-resource "azurerm_mysql_configuration" "sql_mode" {
-  name                = "sql_mode"
-  resource_group_name = azurerm_resource_group.this.name
-  server_name         = azurerm_mysql_server.this.name
-  value               = "STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+  
+resource "azurerm_mysql_flexible_server_configuration" "this" {
+  name                       = "sql_mode"
+  resource_group_name        = azurerm_resource_group.this.name
+  server_name                = azurerm_mysql_flexible_server.this.name
+  value                      = "STRICT_ALL_TABLES,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+  depends_on = [ azurerm_mysql_flexible_server.this ]
 }
