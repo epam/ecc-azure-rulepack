2af6effbf5f35d60782e40684d849c64cad2c912
vit-corp <liashuk.corp@gmail.com>
2024-08-09 02:47:03 +0300

    skip: update CI 169

--- auto_policy_testing/red/keyvault/outputs.tf
+++ auto_policy_testing/red/keyvault/outputs.tf
@@ -1,7 +1,7 @@
 output "keyvault" {
   value = {
     keyvault        = azurerm_key_vault.this.id,
-    keyvault-key    = azurerm_key_vault_key.this.resource_id,
-    keyvault-secret = azurerm_key_vault_secret.this.resource_id
+    keyvault-key    = azurerm_key_vault_key.this.versionless_id,
+    keyvault-secret = azurerm_key_vault_secret.this.versionless_id
   }
 }
--- auto_policy_testing/red/monitor/outputs.tf
+++ auto_policy_testing/red/monitor/outputs.tf
@@ -1,5 +1,5 @@
 output "monitor" {
   value = {
-    monitor-log-profile = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/microsoft.insights/logprofiles/${azurerm_monitor_log_profile.this.name}"
+    monitor-log-profile = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/microsoft.insights/logprofiles/${azurerm_monitor_log_profile.this.name}"
   }
 }
