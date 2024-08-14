a4ea3dde8e968225da2444c67de615969f24da51
vit-corp <liashuk.corp@gmail.com>
2024-08-14 10:09:14 +0300

    skip: update CI 193

--- auto_policy_testing/green/cognitiveservice/cognitiveservices.tf
+++ auto_policy_testing/green/cognitiveservice/cognitiveservices.tf
@@ -2,7 +2,7 @@ resource "azurerm_cognitive_account" "this" {
   name                          = module.naming.resource_prefix.cognitive
   location                      = data.terraform_remote_state.common.outputs.location
   resource_group_name           = data.terraform_remote_state.common.outputs.resource_group
-  kind                          = "Face"
+  kind                          = "OpenAI"
 
   public_network_access_enabled = "false"
 
--- auto_policy_testing/red/batch/batch_account.tf
+++ auto_policy_testing/red/batch/batch_account.tf
@@ -1,6 +1,6 @@
 resource "azurerm_batch_account" "this" {
   name                          = module.naming.resource_prefix.batch
-  location                      = data.terraform_remote_state.common.outputs.location
+  location                      = "eastus"
   resource_group_name           = data.terraform_remote_state.common.outputs.resource_group
   pool_allocation_mode          = "BatchService"
   
