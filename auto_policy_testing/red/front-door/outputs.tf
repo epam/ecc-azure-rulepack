f2af878707fc55d4a5424231afb2767507c56984
vit-corp <liashuk.corp@gmail.com>
2024-08-14 08:48:31 +0300

    skip: update CI 189

--- auto_policy_testing/red/front-door/outputs.tf
+++ auto_policy_testing/red/front-door/outputs.tf
@@ -1,5 +1,6 @@
 output "front-door" {
   value = {
-    front-door = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.Network/frontdoors/${azurerm_frontdoor.this.name}"
+    front-door = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.Network/frontdoors/${azurerm_frontdoor.this.name}",
+    ecc-azure-341-front_door_waf_log4j = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.Network/frontdoors/${azurerm_frontdoor.this1.name}"
   }
 }
--- auto_policy_testing/scripts/exception_rules.py
+++ auto_policy_testing/scripts/exception_rules.py
@@ -15,7 +15,8 @@ azure = {
               "ecc-azure-378-cis_nsg_flow_log_analytics", #policy doesn't work
               "ecc-azure-201-asb_cosmosdb_encrypt_cmk", #policy work but need additional permissions
               "ecc-azure-203-asb_postgresql_encrypt_cmk", #policy work but terraform destroy fails pipeline
-              "ecc-azure-302-redis_cache_disabled_public_access" #python sdk should be updated
+              "ecc-azure-302-redis_cache_disabled_public_access", #python sdk should be updated
+              "ecc-azure-354-acr_anonymous_pull" #issue with policy, should be reviewed and fixed
               ], 
     "red": ["ecc-azure-105-cis_sa_keys_regen", #cannot configure via terraform or az cli
             "ecc-azure-157-asb_mysql_public_access_disabled", #cannot configure via terraform or az cli
@@ -35,7 +36,10 @@ azure = {
             "ecc-azure-058-cis_aks_rbac", #cannot create red infra, screen in tg
             "ecc-azure-235-asb_k8s_policy", #issue with rule, should be fixed
             "ecc-azure-281-aks_non_vulnerable_version", #cannot create red tf because azure restrict to deploy new aks with vulnerable version
-            "ecc-azure-038-cis_log_keyvaults" # policy and tf works but "Azure Policy" automatically deploys DS to keyvault
+            "ecc-azure-038-cis_log_keyvaults", # policy and tf works but "Azure Policy" automatically deploys DS to keyvault
+            "ecc-azure-354-acr_anonymous_pull", #issue with policy, should be reviewed and fixed
+            "ecc-azure-005-cis_sec_email", #cannot create red tf simultaneously with 006, 007 red tf infra
+            "ecc-azure-362-vm_without_va_extension" #cannot create create red tf simultaneously with 094 red tf infra
             ],
     "not-parallel": ["subscription", "spring", "role", "monitor", "defender"],
     "sleep_before_scan": []
