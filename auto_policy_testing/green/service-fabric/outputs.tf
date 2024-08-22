e61900af4ffed3296969567ecfb389a81a573c70
vit-corp <liashuk.corp@gmail.com>
2024-08-23 02:54:14 +0300

    skip: update CI 215

--- auto_policy_testing/green/service-fabric/outputs.tf
+++ auto_policy_testing/green/service-fabric/outputs.tf
@@ -1,5 +1,5 @@
 output "service-fabric" {
   value = {
-    service-fabric-cluster = azurerm_service_fabric_cluster.this.id
+    service-fabric-cluster = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.ServiceFabric/clusters/${azurerm_service_fabric_cluster.name}"
   }
 }
