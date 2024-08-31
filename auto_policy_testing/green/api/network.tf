6cfc0952a525b23ef34acc9de91b5d41c6433335
vit-corp <liashuk.corp@gmail.com>
2024-08-31 16:18:50 +0300

    skip: update CI 225

--- auto_policy_testing/green/api/network.tf
+++ auto_policy_testing/green/api/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "this" {
   name                = module.naming.resource_prefix.vnet
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = data.terraform_remote_state.common.outputs.location
   resource_group_name = data.terraform_remote_state.common.outputs.resource_group
 
@@ -11,6 +11,43 @@ resource "azurerm_subnet" "this" {
   name                 = module.naming.resource_prefix.subnet
   resource_group_name  = data.terraform_remote_state.common.outputs.resource_group
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.0.0/26"]
+  address_prefixes     = ["10.0.1.0/24"]
 
+}
+
+resource "azurerm_network_security_group" "this" {
+  name                = module.naming.resource_prefix.networksecuritygroup
+  location            = data.terraform_remote_state.common.outputs.location
+  resource_group_name = data.terraform_remote_state.common.outputs.resource_group
+
+  security_rule {
+    name                       = "Allow_inbound_tcp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow_inbound_udp"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = module.naming.default_tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
 }
--- auto_policy_testing/green/api/outputs.tf
+++ auto_policy_testing/green/api/outputs.tf
@@ -1,6 +1,6 @@
 output "api" {
   value = {
-    api-management                  = azurerm_api_management.this.id#,
-    #ecc-azure-143-asb_api_mgmt_vnet = azurerm_api_management.this1.id
+    api-management                  = azurerm_api_management.this.id,
+    ecc-azure-143-asb_api_mgmt_vnet = azurerm_api_management.this1.id
   }
 }
