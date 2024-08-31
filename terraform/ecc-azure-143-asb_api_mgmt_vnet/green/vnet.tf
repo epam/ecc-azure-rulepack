79e79f77cbe4c0f7a8043516f35b3213643a0b67
vit-corp <liashuk.corp@gmail.com>
2024-08-31 16:03:48 +0300

    tf-fix: fix terraform for policy 143

--- terraform/ecc-azure-143-asb_api_mgmt_vnet/green/vnet.tf
+++ terraform/ecc-azure-143-asb_api_mgmt_vnet/green/vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "this" {
   name                = "vnet-${var.prefix}"
-  address_space       = ["10.0.0.0/24"]
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
@@ -11,6 +11,42 @@ resource "azurerm_subnet" "this" {
   name                 = "snet1-${var.prefix}"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.0.0/26"]
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.prefix}-nsg-green"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "Allow_inbound_tcp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow_inbound_udp"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
 }
