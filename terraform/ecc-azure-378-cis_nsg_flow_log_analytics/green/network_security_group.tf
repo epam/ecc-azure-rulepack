8651a824101c2398665cf279ed7bca0109d98303
vit-corp <liashuk.corp@gmail.com>
2024-09-04 01:08:47 +0300

    tf-fix: fix terraform for policy 378

--- terraform/ecc-azure-378-cis_nsg_flow_log_analytics/green/network_security_group.tf
+++ terraform/ecc-azure-378-cis_nsg_flow_log_analytics/green/network_security_group.tf
@@ -6,6 +6,14 @@ resource "azurerm_network_security_group" "this" {
   tags = var.tags
 }
 
+resource "azurerm_network_security_group" "this1" {
+  name                = "${var.prefix}-nsg-green1"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this1.name
+
+  tags = var.tags
+}
+
 resource "azurerm_network_watcher" "this" {
   name                = "ecc-azure-378-cisnsgflowloganalytics-nw"
   location            = azurerm_resource_group.this.location
@@ -36,6 +44,28 @@ resource "azurerm_network_watcher_flow_log" "this" {
   name                 = "ecc-azure-378-cisnsgflowloganalytics-log"
 
   network_security_group_id = azurerm_network_security_group.this.id
+  storage_account_id        = azurerm_storage_account.this.id
+  enabled                   = true
+
+  retention_policy {
+    enabled = true
+    days    = 7
+  }
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = azurerm_log_analytics_workspace.this.workspace_id
+    workspace_region      = azurerm_log_analytics_workspace.this.location
+    workspace_resource_id = azurerm_log_analytics_workspace.this.id
+    interval_in_minutes   = 10
+  }
+}
+
+resource "azurerm_network_watcher_flow_log" "this1" {
+  network_watcher_name = azurerm_network_watcher.this.name
+  resource_group_name  = azurerm_resource_group.this.name
+  name                 = "ecc-azure-378-cisnsgflowloganalytics-log1"
+
+  network_security_group_id = azurerm_network_security_group.this1.id
   storage_account_id        = azurerm_storage_account.this.id
   enabled                   = true
 
--- terraform/ecc-azure-378-cis_nsg_flow_log_analytics/green/resource_group.tf
+++ terraform/ecc-azure-378-cis_nsg_flow_log_analytics/green/resource_group.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "this" {
   name     = "${var.prefix}-rg-green"
   location = var.location
 }
+
+resource "azurerm_resource_group" "this1" {
+  name     = "${var.prefix}-rg-green1"
+  location = var.location
+}
