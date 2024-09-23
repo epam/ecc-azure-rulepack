4695a7f035d8430850b5fc3fc64027d06af76e36
vit-corp <liashuk.corp@gmail.com>
2024-09-24 02:33:00 +0300

    skip: update CI 265

--- auto_policy_testing/green/automation/automation_account.tf
+++ auto_policy_testing/green/automation/automation_account.tf
@@ -1,6 +1,6 @@
 resource "azurerm_automation_account" "this" {
-  name                          = module.naming.resource_prefix.automation
-  location                      = "eastasia"
+  name                          = "${module.naming.resource_prefix.automation}${random_integer.this.result}"
+  location                      = "eastus"
   resource_group_name           = data.terraform_remote_state.common.outputs.resource_group
   sku_name                      = "Basic"
   tags                          = module.naming.default_tags
