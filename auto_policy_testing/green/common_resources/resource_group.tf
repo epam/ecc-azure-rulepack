02580565c33a83e70ddacdee6e2bde44b7803b52
vit-corp <liashuk.corp@gmail.com>
2024-09-26 02:34:50 +0300

    skip: update CI 276

--- auto_policy_testing/green/common_resources/resource_group.tf
+++ auto_policy_testing/green/common_resources/resource_group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "this" {
-  name     = module.naming.resource_prefix.resourcegroup
+  name     = "${module.naming.resource_prefix.resourcegroup}1"
   location = "${var.location}"
 
   tags = module.naming.default_tags
