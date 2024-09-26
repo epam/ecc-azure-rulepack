308ac42b8728da90de8276f35c9e22edb8f4af09
vit-corp <liashuk.corp@gmail.com>
2024-09-27 00:10:44 +0300

    skip: update CI 287

--- auto_policy_testing/red/common_resources/key_vault.tf
+++ auto_policy_testing/red/common_resources/key_vault.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "this" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "premium"
   soft_delete_retention_days = 7
-  purge_protection_enabled   = false
+  purge_protection_enabled   = true
   enabled_for_disk_encryption = true
 
   tags = module.naming.default_tags
