ef2569b2ae72390ee38c4a0b5f1199d875ee0b08
vit-corp <liashuk.corp@gmail.com>
2024-09-27 18:17:58 +0300

    skip: update CI 291

--- auto_policy_testing/green/defender/security_center_pricing.tf
+++ auto_policy_testing/green/defender/security_center_pricing.tf
@@ -13,10 +13,10 @@ resource "azurerm_security_center_subscription_pricing" "azure_sql" {
   resource_type = "SqlServers"
 }
 
-#resource "azurerm_security_center_subscription_pricing" "sql_machines" {
-#  tier          = "Standard"
-#  resource_type = "SqlServerVirtualMachines"
-#}
+resource "azurerm_security_center_subscription_pricing" "sql_machines" {
+  tier          = "Standard"
+  resource_type = "SqlServerVirtualMachines"
+}
 
 resource "azurerm_security_center_subscription_pricing" "storages" {
   tier          = "Standard"
@@ -48,13 +48,13 @@ resource "azurerm_security_center_subscription_pricing" "arm" {
   resource_type = "Arm"
 }
 
-#resource "azurerm_security_center_subscription_pricing" "os_rds" {
-#  tier          = "Standard"
-#  resource_type = "OpenSourceRelationalDatabases"
-#}
+resource "azurerm_security_center_subscription_pricing" "os_rds" {
+  tier          = "Standard"
+  resource_type = "OpenSourceRelationalDatabases"
+}
 
-#resource "azurerm_security_center_subscription_pricing" "cosmodb" {
-#  tier          = "Standard"
-#  resource_type = "CosmosDbs"
-#}
+resource "azurerm_security_center_subscription_pricing" "cosmodb" {
+  tier          = "Standard"
+  resource_type = "CosmosDbs"
+}
 
