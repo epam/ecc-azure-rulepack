934dd8be1b2d1f67f5b4627ef9f3b04c35acbfc8
vit-corp <liashuk.corp@gmail.com>
2024-11-08 02:19:20 +0200

    new: add policy ecc-azure-426-dep_nsg_w_flow_logs

---
+++ terraform/ecc-azure-426-dep_nsg_w_flow_logs/green/network_security_group.tf
@@ -0,0 +1,8 @@
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.prefix}-nsg-green"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  tags = var.tags
+}
+
