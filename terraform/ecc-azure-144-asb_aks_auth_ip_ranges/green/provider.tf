2897e406b58889f2affa18a07a58c2cb7c86526e
vit-corp <liashuk.corp@gmail.com>
2025-01-24 00:42:06 +0200

    tf-fix: fix terraform for policy 144

--- terraform/ecc-azure-144-asb_aks_auth_ip_ranges/green/provider.tf
+++ terraform/ecc-azure-144-asb_aks_auth_ip_ranges/green/provider.tf
@@ -2,18 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>4"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "azurerm_resource_group" "this" {
-  name     = "${var.prefix}-rg-green"
-  location = var.location
-
-  tags = var.tags
 }
--- terraform/ecc-azure-144-asb_aks_auth_ip_ranges/red/provider.tf
+++ terraform/ecc-azure-144-asb_aks_auth_ip_ranges/red/provider.tf
@@ -2,18 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>4"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "azurerm_resource_group" "this" {
-  name     = "${var.prefix}-rg-red"
-  location = var.location
-
-  tags = var.tags
 }
