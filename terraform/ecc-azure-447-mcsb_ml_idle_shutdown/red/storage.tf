da60ef63ed1bfdbf647373665aca1b67cc45e4ef
vit-corp <liashuk.corp@gmail.com>
2025-01-26 22:10:40 +0200

    new: add policy ecc-azure-447-mcsb_ml_idle_shutdown

---
+++ terraform/ecc-azure-447-mcsb_ml_idle_shutdown/red/storage.tf
@@ -0,0 +1,9 @@
+resource "azurerm_storage_account" "this" {
+  name                     = "storage${var.prefix}redcustodian"
+  location                 = azurerm_resource_group.this.location
+  resource_group_name      = azurerm_resource_group.this.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+  tags                     = var.tags
+}
---
+++ terraform/ecc-azure-447-mcsb_ml_idle_shutdown/red/terraform.tfvars
@@ -0,0 +1,8 @@
+prefix = "447"
+
+location = "eastus"
+
+tags = {
+  CustodianRule    = "ecc-azure-447-mcsb_ml_idle_shutdown"
+  ComplianceStatus = "Red"
+}
