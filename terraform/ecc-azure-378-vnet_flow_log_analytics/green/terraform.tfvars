6cbf4efb309c22894fb7271cad3a34c2d2b6c3ec
vit-corp <liashuk.corp@gmail.com>
2025-01-26 23:08:04 +0200

    upd: update policy 378

--- terraform/ecc-azure-378-vnet_flow_log_analytics/green/terraform.tfvars
+++ terraform/ecc-azure-378-vnet_flow_log_analytics/green/terraform.tfvars
@@ -3,6 +3,6 @@ prefix = "378"
 location = "centralus"
 
 tags = {
-  CustodianRule    = "ecc-azure-378-cis_nsg_flow_log_analytics"
+  CustodianRule    = "ecc-azure-378-vnet_flow_log_analytics"
   ComplianceStatus = "Green"
 }
--- terraform/ecc-azure-378-vnet_flow_log_analytics/green/vnet.tf
+++ terraform/ecc-azure-378-vnet_flow_log_analytics/green/vnet.tf
@@ -1,41 +1,51 @@
-resource "azurerm_network_security_group" "this" {
-  name                = "${var.prefix}-nsg-green"
+resource "azurerm_network_watcher" "this" {
+  name                = "ecc-azure-${var.prefix}-vnetflowloganalytics-nw"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
   tags = var.tags
 }
 
-resource "azurerm_network_watcher" "this" {
-  name                = "ecc-azure-378-cisnsgflowloganalytics-nw"
+resource "azurerm_virtual_network" "this" {
+  name                = "${var.prefix}vnetgreen"
+  address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+  
+  tags                = var.tags
 }
 
+
+
 resource "azurerm_storage_account" "this" {
-  name                = "eccazure378sa"
+  name                = "eccazure${var.prefix}sa"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 
   account_tier              = "Standard"
   account_kind              = "StorageV2"
   account_replication_type  = "LRS"
-  enable_https_traffic_only = true
+  allow_nested_items_to_be_public = false
+
+  tags = var.tags
 }
 
 resource "azurerm_log_analytics_workspace" "this" {
-  name                = "ecc-azure-378-cisnsgflowloganalytics-law"
+  name                = "ecc-azure-${var.prefix}-vnetflowloganalytics-law"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   sku                 = "PerGB2018"
+
+  tags = var.tags
 }
 
 resource "azurerm_network_watcher_flow_log" "this" {
   network_watcher_name = azurerm_network_watcher.this.name
   resource_group_name  = azurerm_resource_group.this.name
-  name                 = "ecc-azure-378-cisnsgflowloganalytics-log"
+  name                 = "ecc-azure-${var.prefix}-vnetflowloganalytics-log"
 
-  network_security_group_id = azurerm_network_security_group.this.id
+  target_resource_id   = azurerm_virtual_network.this.id
+
   storage_account_id        = azurerm_storage_account.this.id
   enabled                   = true
 
@@ -50,5 +60,7 @@ resource "azurerm_network_watcher_flow_log" "this" {
     workspace_resource_id = azurerm_log_analytics_workspace.this.id
     interval_in_minutes   = 10
   }
+
+  tags = var.tags
 }
 
