8c8d068115725751a6d8c6fcaf06eb6a3e686c3b
vit-corp <liashuk.corp@gmail.com>
2025-03-05 01:16:24 +0200

    skip: update CI 306

--- auto_policy_testing/green/front-door/outputs.tf
+++ auto_policy_testing/green/front-door/outputs.tf
@@ -1,5 +1,6 @@
 output "front-door" {
   value = {
-    front-door = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.Network/frontdoors/${azurerm_frontdoor.this.name}"
+    front-door = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.terraform_remote_state.common.outputs.resource_group}/providers/Microsoft.Network/frontdoors/${azurerm_frontdoor.this.name}",
+    ecc-azure-431-dep_retired_frontdoor_classic = azurerm_cdn_frontdoor_profile.this.id
   }
 }
