0feb8980fb9967ec52e96fc931d0ffbc7aada7cd
vit-corp <liashuk.corp@gmail.com>
2025-04-15 01:51:56 +0300

    new: add policy ecc-azure-463-cis_sa_cross_tenant_replication_disabled

---
+++ terraform/ecc-azure-463-cis_sa_cross_tenant_replication_disabled/red/storage_account.tf
@@ -0,0 +1,12 @@
+resource "azurerm_storage_account" "this" {
+  name                     = "sa${var.prefix}red"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  allow_nested_items_to_be_public = false
+
+  cross_tenant_replication_enabled = true
+
+  tags = var.tags
+}
