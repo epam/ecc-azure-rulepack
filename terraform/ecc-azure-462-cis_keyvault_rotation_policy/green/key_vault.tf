c10cfcccdb0e6b195d896a2685dbb4a8b93d9c6d
vit-corp <liashuk.corp@gmail.com>
2025-04-17 01:10:03 +0300

    skip: Merge branch 'main' into feature/new_459

---
+++ terraform/ecc-azure-462-cis_keyvault_rotation_policy/green/key_vault.tf
@@ -0,0 +1,51 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "this" {
+  name                       = "kv${var.prefix}green${random_integer.this.result}"
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+  
+
+
+  tags = var.tags
+}
+
+resource "azurerm_key_vault_access_policy" "client" {
+  key_vault_id = azurerm_key_vault.this.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions    = ["Get", "Create", "Delete", "List", "Restore", "Recover", "UnwrapKey", "WrapKey", "Purge", "Encrypt", "Decrypt", "Sign", "Verify", "GetRotationPolicy", "SetRotationPolicy"]
+  secret_permissions = ["Get"]
+}
+
+resource "azurerm_key_vault_key" "this" {
+  name         = "key${var.prefix}green${random_integer.this.result}"
+  key_vault_id = azurerm_key_vault.this.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
+
+  depends_on = [ azurerm_key_vault_access_policy.client ]
+}
---
+++ terraform/ecc-azure-462-cis_keyvault_rotation_policy/green/terraform.tfvars
@@ -0,0 +1,8 @@
+prefix = "462"
+
+location = "eastus"
+
+tags = {
+  CustodianRule    = "ecc-azure-462-cis_keyvault_rotation_policy"
+  ComplianceStatus = "Green"
+}
