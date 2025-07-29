c1f177d33b8f15de9b6f95481ace6c124f0fb2cd
vit-corp <liashuk.corp@gmail.com>
2025-07-29 23:47:23 +0300

    new: add policy ecc-azure-499-cis_mysql_db_resource_locks

---
+++ terraform/ecc-azure-499-cis_mysql_db_resource_locks/green/mysql.tf
@@ -0,0 +1,18 @@
+resource "azurerm_mysql_flexible_server" "this" {
+  name                = "${var.prefix}-mysqlserver-green"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  administrator_login          = random_string.this.result
+  administrator_password       = random_password.this.result
+  sku_name                     = "GP_Standard_D2ds_v4"
+
+  tags = var.tags
+}
+
+resource "azurerm_management_lock" "this" {
+  name       = "nsg${var.prefix}greenlock"
+  scope      = azurerm_mysql_flexible_server.this.id
+  lock_level = "ReadOnly"
+  notes      = "This MYSQL Flexible Server can't be edited!"
+}
