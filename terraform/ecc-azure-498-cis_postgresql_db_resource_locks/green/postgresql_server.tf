a85545ad7627c7d55f9cac5c07155680b63f1b18
vit-corp <liashuk.corp@gmail.com>
2025-07-29 23:47:00 +0300

    new: add policy ecc-azure-498-cis_postgresql_db_resource_locks

---
+++ terraform/ecc-azure-498-cis_postgresql_db_resource_locks/green/postgresql_server.tf
@@ -0,0 +1,23 @@
+resource "azurerm_postgresql_flexible_server" "this" {
+  name                = "${var.prefix}-postgresqlserver-green"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  version                       = "12"
+  public_network_access_enabled = false
+  administrator_login           = random_string.this.result
+  administrator_password        = random_password.this.result
+
+  storage_mb   = 32768
+
+  sku_name   = "B_Standard_B1ms"
+
+  tags = var.tags
+}
+
+resource "azurerm_management_lock" "this" {
+  name       = "nsg${var.prefix}greenlock"
+  scope      = azurerm_postgresql_flexible_server.this.id
+  lock_level = "ReadOnly"
+  notes      = "This Postgresql Server can't be edited!"
+}
