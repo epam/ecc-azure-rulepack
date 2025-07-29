2eb98dba78ee26dbc06d28e44a2345e6b0c7ce01
vit-corp <liashuk.corp@gmail.com>
2025-07-29 23:46:21 +0300

    new: add policy ecc-azure-497-cis_mssql_server_resource_locks

---
+++ terraform/ecc-azure-497-cis_mssql_server_resource_locks/green/random.tf
@@ -0,0 +1,11 @@
+resource "random_password" "this" {
+  length  = 13
+  special = true
+  numeric  = true
+}
+
+resource "random_string" "this" {
+  length  = 8
+  numeric  = false
+  special = false
+}
