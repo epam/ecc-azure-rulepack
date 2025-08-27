c0b64fe05bfa08e7c1558e384a7e8e9e05510063
vit-corp <liashuk.corp@gmail.com>
2025-08-27 09:35:46 +0300

    new: add policy ecc-azure-438-cis_sa_grs

---
+++ terraform/ecc-azure-438-cis_sa_grs/green/storage_account.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "this" {
+  name                     = "sa${var.prefix}green"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  allow_nested_items_to_be_public = false
+
+  tags = var.tags
+}
+
---
+++ terraform/ecc-azure-438-cis_sa_grs/green/terraform.tfvars
@@ -0,0 +1,8 @@
+prefix = "438"
+
+location = "eastus"
+
+tags = {
+  CustodianRule    = "ecc-azure-438-cis_sa_grs"
+  ComplianceStatus = "Green"
+}
