70e64316d2deac7c5500e24fb8b580d1ca9057ba
vit-corp <liashuk.corp@gmail.com>
2025-09-08 22:46:17 +0300

    new: add policy ecc-azure-527-cis_cosmosdb_entra_auth

---
+++ terraform/ecc-azure-527-cis_cosmosdb_entra_auth/green/random_int.tf
@@ -0,0 +1,4 @@
+resource "random_integer" "this" {
+  min = 100
+  max = 2000
+}
---
+++ terraform/ecc-azure-527-cis_cosmosdb_entra_auth/red/cosmosdb.tf
@@ -0,0 +1,19 @@
+resource "azurerm_cosmosdb_account" "this" {
+  name                = "cdb${random_integer.this.result}red"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+  geo_location {
+    location          = azurerm_resource_group.this.location
+    failover_priority = 0
+  }
+  local_authentication_disabled = false
+
+}
