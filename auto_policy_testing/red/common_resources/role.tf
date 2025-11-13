8024545e2e1a10f2c580d8e7fb0d87425e0fc418
vit-corp <liashuk.corp@gmail.com>
2025-11-13 04:03:53 +0100

    CI 406

--- auto_policy_testing/red/common_resources/role.tf
+++ auto_policy_testing/red/common_resources/role.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "this" {
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "this1" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
