be7658dd46e8142f32a17b147654e43d24d20502
vit-corp <liashuk.corp@gmail.com>
2025-11-16 14:55:30 +0100

    CI 407

--- auto_policy_testing/red/common_resources/role.tf
+++ auto_policy_testing/red/common_resources/role.tf
@@ -1,11 +1,11 @@
-resource "azurerm_role_assignment" "this" {
-  scope                = azurerm_resource_group.resources.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
+#resource "azurerm_role_assignment" "this" {
+#  scope                = azurerm_resource_group.resources.id
+#  role_definition_name = "Key Vault Administrator"
+#  principal_id         = data.azurerm_client_config.current.object_id
+#}
 
-resource "azurerm_role_assignment" "this1" {
-  scope                = azurerm_resource_group.this.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
+#resource "azurerm_role_assignment" "this1" {
+#  scope                = azurerm_resource_group.this.id
+#  role_definition_name = "Key Vault Administrator"
+#  principal_id         = data.azurerm_client_config.current.object_id
+#}
